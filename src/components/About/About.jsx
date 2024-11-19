47a6ea941f8bc6ff1b4cae280923d0ae68c7bcc9
Jaimie <INVPMSDEV-02>
2024-11-19 13:10:29 +0800

    modified about, added new icons, removed old and not needed icons.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
+
+import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
-    <section>
-        <h2>About</h2>
-        <div>
+    <section className={styles.container} id="about"> 
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
             <img 
                 src={getImageUrl("about/aboutImage.png")} 
                 alt=""
+                className={styles.aboutImage}
             />
-            <ul>
-                <li>
+            <ul className={styles.aboutItems}>
+                <li className={styles.aboutItem}>
                     <img
-                        src={getImageUrl("about/cursorIcon.png")} 
-                        alt=""
+                        src={getImageUrl("about/pcSystemIcon.png")} 
+                        alt="Desktop Application"
                     />
+                    <div className={styles.aboutItemText}>
+                        <h3>Desktop Sytem Developer</h3>
+                        <p>
+                            Etiam non justo efficitur, tincidunt sem vel, tincidunt dolor. 
+                            Mauris sed ipsum porttitor, semper arcu eu, ultrices elit.
+                        </p>
+                    </div>
+                </li>
+                <li className={styles.aboutItem}>
+                    <img
+                        src={getImageUrl("about/dbIcon.png")} 
+                        alt="Database Admin"
+                    />
+                    <div className={styles.aboutItemText}>
+                        <h3>Database Designer</h3>
+                        <p>
+                            Proin blandit vulputate tincidunt. In convallis dapibus pretium. 
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                    </div>
                 </li>
             </ul>
         </div>
